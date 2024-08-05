65ceccde89b25697f32994f72c6937681fd8825e
= <=>
2024-08-05 18:14:57 +0000

    linked list start

---
+++ linked_list.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+    def print_list(self):
+        current_node = self.head
+        while current_node.next:
+            print(current_node.data,end="->")
+            current_node = current_node.next
+        print(current_node.data)
+
+ll = LinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.print_list()
+
+
+

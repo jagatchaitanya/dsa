71e8d9e459534ddb2f529879c483fb8a192b0d17
jagatchaitanya <jagat.prabhala@gmail.com>
2024-08-08 18:39:17 +0530

    added positional insert

--- linked_list.py
+++ linked_list.py
@@ -16,6 +16,22 @@ class LinkedList:
         while last_node.next:
             last_node = last_node.next
         last_node.next = new_node
+    def prepend(self,value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+    def position_insert(self,position,value):
+        new_node=Node(value)
+        counter = 0
+        current_node = self.head
+        while current_node.next:
+            counter = counter + 1
+            if counter == position:
+                break
+            current_node = current_node.next
+        temp = current_node.next
+        current_node.next = new_node
+        current_node.next.next = temp
     def print_list(self):
         current_node = self.head
         while current_node.next:
@@ -27,7 +43,15 @@ ll = LinkedList()
 ll.append(1)
 ll.append(2)
 ll.append(3)
+ll.append(4)
 ll.print_list()
-
+ll.prepend(-1)
+ll.prepend(-2)
+ll.prepend(-3)
+ll.prepend(-4)
+ll.print_list()
+ll.position_insert(3,0.6)
+ll.position_insert(3,0.7)
+ll.print_list()
 
 

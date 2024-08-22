8a3af68cf1768b0dca6a50f4c03393b917815646
jagatchaitanya <jagat.prabhala@gmail.com>
2024-08-22 23:33:12 +0530

    midpoint finding in linkedlist

---
+++ midpoint_linkedlist.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+def midpoint_linkedlist(head):
+    fast = head
+    slow = head
+    while fast:
+        slow = slow.next
+        fast = fast.next.next
+    return slow
+
+if __name__ == "__main__":
+    ll  = Node(1)
+    ll.next = Node(2)
+    ll.next.next = Node(3)
+    ll.next.next.next = Node(4)
+    ll.next.next.next.next = Node(5)
+    ll.next.next.next.next.next = Node(6)
+    print(midpoint_linkedlist(ll).data)
